33670dd2abab93e09e4e0e84cd3c8ff2252474f9
Oleg Zilberman <olegzilberman.zilberman@gmail.com>
2022-10-06 18:26:37 -0700

    code cleanup. also modified call to cowboy_req:match_qs() to filter calls without a start/end date. need to add an error return if cowboy_req:match_qs() fails.

--- src/star_watch_handler.erl
+++ src/star_watch_handler.erl
@@ -5,18 +5,45 @@
 -include("include/apod_record_def.hrl").
 
 init(Req0, State) ->
-    Parsed = parse_request(Req0),
-    Req = cowboy_req:reply(200,
-        #{<<"content-type">> => <<"application/json">>},
-        Parsed,
-        Req0),
+    Req = handle(Req0, State),
     {ok, Req, State}.
 
+
+handle(Req, State) -> 
+  case cowboy_req:method(Req) of
+    <<"POST">> -> 
+      Body = cowboy_req:has_body(Req),
+      Request = postMethod(<<"POST">>, Body, Req),
+        {ok, Request, State};
+    <<"GET">> -> 
+      #{id := Id} = cowboy_req:match_qs([{id, [], undefined}], Req),
+      Request = getMethod(<<"GET">>, Id, Req),
+        {ok, Request, State};
+    <<"PUT">> -> 
+      Body = cowboy_req:has_body(Req),
+      Request = putMethod(<<"PUT">>, Body, Req),
+        {ok, Request, State}
+  end.
+
+  postMethod(<<"POST">>, _Body, Req) -> 
+    cowboy_req:reply(200,  #{<<"content-type">> => <<"application/json; charset=utf-8">>}, parse_request(Req), Req).
+
+  getMethod(<<"GET">>, _Id, Req) -> 
+    cowboy_req:reply(200,  #{<<"content-type">> => <<"application/json; charset=utf-8">>}, parse_request(Req), Req).
+
+  putMethod(<<"PUT">>, _Body, Req) -> 
+    cowboy_req:reply(200,  #{<<"content-type">> => <<"application/json; charset=utf-8">>}, parse_request(Req), Req).
+
+
+%%% 
+%%% Add error handling to the return value of cowboy_req(). 
+%%% If start_date or end_date are empty the function will return {badkey,start_date} or {badkey,end_date}
+%%% A json encoded error message should be returned to the caller.
 parse_request(Request) ->
     #{
         start_date  := StartDate,
-        end_date    := EndDate
-    } = cowboy_req:match_qs([start_date, end_date], Request),
+        end_date    := EndDate %{Name, Constraints, Default}
+    } = cowboy_req:match_qs([{start_date, nonempty}, {end_date, nonempty}], Request),
     process_date_request(StartDate, EndDate, Request).
 
 process_date_request(StartDate, EndDate, Req) ->
