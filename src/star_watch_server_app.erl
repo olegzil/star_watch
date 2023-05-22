7392654db9687e64130256c2b38faa45f9838594
oleg zilberman <olegzilberman.zilberman@gmail.com>
2023-05-21 17:50:48 -0700

    replaced port number with macro

--- src/star_watch_server_app.erl
+++ src/star_watch_server_app.erl
@@ -31,7 +31,7 @@ start(_Type, _Args) ->
         {'_', [FetchAdminRoute, FetchYoutubeChanneRoute, FetchNasaImagesRoute, FetchApodRoute, TelemetryRoute, RegistrationRoute, CatchAllRoute]}
     ]),
     {ok, _} = cowboy:start_clear(star_watch_http_listener,
-         [{port,8083}],
+         [{port, ?HTTP_PORT}],
         #{env => #{dispatch => Dispatch}}
     ),
     inets:start(),
